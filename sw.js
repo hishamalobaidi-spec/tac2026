e97ecfef126836be3fd3a69fe5b893a7701c27ee
hishamalobaidi-spec <hisham.alobaidi@gmail.com>
2026-01-28 06:33:54 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,52 +1,55 @@
-const CACHE_NAME = 'tac2026-v7';
+const CACHE_NAME = 'tac2026-v8';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
+// Install - cache files
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .catch(err => console.log('Cache error:', err))
-    );
-    self.skipWaiting();
+  self.skipWaiting(); // Activate immediately
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+// Activate - delete old caches
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Delete old versions
+          }
         })
-    );
-    self.clients.claim();
+      );
+    }).then(() => {
+      return self.clients.claim(); // Take control immediately
+    })
+  );
 });
 
+// Fetch - Network first, then cache (always try to get fresh content)
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) return response;
-                return fetch(event.request).then(response => {
-                    if (!response || response.status !== 200) {
-                        return response;
-                    }
-                    const responseToCache = response.clone();
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, responseToCache);
-                    });
-                    return response;
-                });
-            })
-            .catch(() => caches.match('./index.html'))
-    );
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Got network response, cache it
+        if (response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        // Network failed, try cache
+        return caches.match(event.request);
+      })
+  );
 });
